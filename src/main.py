efb246c84a3726e777d53eef8003668b1aca71a3
prajaldeqode <prapatidar@deqode.com>
2024-07-23 15:19:30 +0530

    refactor multiple connection

--- src/main.py
+++ src/main.py
@@ -23,32 +23,50 @@ graphnode_arbitrum_config = {
     'port': int(os.environ.get("GRAPH_NODE_ARBITRUM_PORT"))
 }
 
-def fetch_vids(db_config, table, column):
+def createConnection(db_config):
     connection = psycopg2.connect(**db_config)
-    cursor = connection.cursor()
-    query = f"SELECT {column} FROM {table} order by {column} desc limit 1"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    connection.close()
-    print(result)
-    return [row[0] for row in result]
+    return connection
 
-def calculate_difference(vids1, vids2):
-    return abs(sum(vids1) - sum(vids2))
+citus_connection = createConnection(citus_config)
+graphnode_connection = createConnection(graphnode_arbitrum_config)
+
+def fetch_data(fetchFor, db_config, table, column):
+    global citus_connection
+    global graphnode_connection
+    connection = citus_connection if fetchFor == "citus" else graphnode_connection
+    for itr in range(3):
+        try:
+            query = f"SELECT {column} FROM {table} order by {column} desc limit 1"
+            cursor = connection.cursor()
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result[0][0]
+        except Exception as e:
+            if(itr == 2):
+                raise e
+            if(fetchFor == "citus"):
+                citus_connection = createConnection(db_config=db_config)
+                connection = citus_connection
+            else:
+                graphnode_connection = createConnection(db_config=graphnode_arbitrum_config)
+                connection = graphnode_connection
+
+def calculate_difference(vid1, vid2):
+    return abs(vid1 - vid2)
     
 def job():
     for table in tables:
         try:
-            citus_vids = fetch_vids(db_config=citus_config, table=table["table"], column=table["column"])
-            graphnode_arbitrum_vids = fetch_vids(db_config=graphnode_arbitrum_config, table=table["table"], column=table["column"])
-            print(table["table"],citus_vids,graphnode_arbitrum_vids)
-            if(type(citus_vids) != list):
+            citus_data = fetch_data(fetchFor = "citus", db_config=citus_config, table=table["table"], column=table["column"])
+            graphnode_arbitrum_data = fetch_data(fetchFor = "graphnod_arbitrum", db_config=graphnode_arbitrum_config, table=table["table"], column=table["column"])
+            print(table["table"],citus_data,graphnode_arbitrum_data)
+            if(type(citus_data) != int):
                 raise Exception(f"Data is not fetched from {table['table']} table in citus database")
-            if(type(graphnode_arbitrum_vids) != list):
+            if(type(graphnode_arbitrum_data) != int):
                 raise Exception(f"Data is not fetched from {table['table']} table in graph_node_arbitrum database")
             expose_difference_in_vid(
                 table_name=table["table"] ,
-                difference=calculate_difference(vids1=citus_vids,vids2=graphnode_arbitrum_vids)
+                difference=calculate_difference(vid1=citus_data,vid2=graphnode_arbitrum_data)
             )
         except Exception as e:
             print(e)
